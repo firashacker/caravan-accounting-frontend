4e18be0f76bdeedd7991f2111e09b08c2f54c7b6
firashacker <firastom0@gmail.com>
2025-08-02 01:28:09 +0300

    converted to react

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-});
+  plugins: [react()],
+})
